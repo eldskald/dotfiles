12d31ca24cc2fdc385c4b7c07b909411abf0ecac
eldskald <rafaelbordoni00@gmail.com>
2025-07-11 03:02:35 -0300

    Add preview to find_dirs custom extension

--- nvim/lua/telescope/_extensions/find_dirs.lua
+++ nvim/lua/telescope/_extensions/find_dirs.lua
@@ -1,6 +1,7 @@
 local pickers = require('telescope.pickers')
 local finders = require('telescope.finders')
 local conf = require('telescope.config').values
+local previewers = require('telescope.previewers')
 
 local find_dirs = function(opts)
     opts = opts or {}
@@ -9,6 +10,12 @@ local find_dirs = function(opts)
             prompt_title = 'Find Directories',
             finder = finders.new_oneshot_job({ 'fd', '-t=d' }, opts),
             sorter = conf.generic_sorter(opts),
+            previewer = previewers.new_termopen_previewer({
+                title = 'Directory Preview',
+                get_command = function(entry, _)
+                    return { 'ls', '-1', entry.value }
+                end,
+            }),
         })
         :find()
 end
